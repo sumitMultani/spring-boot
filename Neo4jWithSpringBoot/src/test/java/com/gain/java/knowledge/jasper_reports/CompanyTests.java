bbeed1f85bb4c3d09a24e5b40a83074cf3d29669
sumitpal.singh <sumitpal.singh@cloudsmartz.net>
2019-12-26 22:01:02 +0530

    test class committed

---
+++ Neo4jWithSpringBoot/src/test/java/com/gain/java/knowledge/jasper_reports/CompanyTests.java
@@ -0,0 +1,13 @@
+package com.gain.java.knowledge.jasper_reports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
